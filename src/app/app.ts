9720300a83270ad1f65b08713d4495f9e7cd46ed
vanhoa690 <vanhoa690@gmail.com>
2025-06-26 15:47:19 +0700

    demo day1

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HelloWorld } from './hello-world/hello-world';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HelloWorld],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
   protected title = 'angular-su25';
