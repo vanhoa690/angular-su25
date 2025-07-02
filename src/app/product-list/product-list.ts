085d22cede2cd36e322f60137c899ee8f224ed61
vanhoa690 <vanhoa690@gmail.com>
2025-07-02 10:22:08 +0700

    demo day 1

---
+++ src/app/product-list/product-list.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+@Component({
+  selector: 'app-product-list',
+  imports: [FormsModule, CommonModule],
+  templateUrl: './product-list.html',
+  styleUrl: './product-list.css',
+})
+export class ProductList {
+  title = 'Product List Page';
+  isAvailable = true;
+  productName = '';
+  products = [
+    { id: 1, name: 'Laptop', price: 1000, inStock: true },
+    { id: 2, name: 'Phone', price: 500, inStock: false },
+    { id: 3, name: 'Tablet', price: 300, inStock: true },
+  ];
+  handleClick() {
+    alert('Button clicked!');
+  }
+
+  filterText = '';
+
+  filterProducts() {
+    return this.products.filter((product) =>
+      product.name.toLowerCase().includes(this.filterText.toLowerCase())
+    );
+  }
+}
