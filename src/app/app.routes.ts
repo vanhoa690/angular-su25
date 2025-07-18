33f8db393d1ed71a522d20e2b38e54f6f43e2ac0
vanhoa690 <vanhoa690@gmail.com>
2025-07-18 12:46:19 +0700

    routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,29 @@
 import { Routes } from '@angular/router';
 import { ProductList } from './product-list/product-list';
 import { ProductDetail } from './product-detail/product-detail';
+import { CategoryList } from './category-list/category-list';
 
 export const routes: Routes = [
+  {
+    path: '',
+    component: ProductList,
+  },
   {
     path: 'products',
     component: ProductList,
   },
+  {
+    path: 'categories',
+    component: CategoryList,
+  },
+  //   {
+  //   path: 'brands',
+  //   component: BrandList,
+  // },
+  //   {
+  //   path: 'users',
+  //   component: UserList,
+  // },
   {
     path: 'products/:id',
     component: ProductDetail,
