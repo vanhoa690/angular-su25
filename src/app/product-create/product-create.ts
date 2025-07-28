b13ff13e63a96e93f11bb90ece0981b5febc8d56
vanhoa690 <vanhoa690@gmail.com>
2025-07-28 09:43:41 +0700

    tesst

--- src/app/product-create/product-create.ts
+++ src/app/product-create/product-create.ts
@@ -1,35 +1,34 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule, NgForm } from '@angular/forms';
-import { ProductService } from '../services/product.service';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-product-create',
-  imports: [FormsModule, CommonModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './product-create.html',
   styleUrl: './product-create.css',
 })
 export class ProductCreate {
-  product = {
-    name: '',
-    image: '',
-    price: 0,
-    inStock: false,
-  };
+  productForm: FormGroup;
 
-  constructor(private productService: ProductService) {}
+  constructor(private fb: FormBuilder) {
+    this.productForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+      image: ['', Validators.required],
+      price: [0, [Validators.required, Validators.min(0)]],
+      inStock: [false],
+    });
+  }
 
-  onSubmit(form: NgForm) {
-    if (form.valid) {
-      console.log('Form Submitted:', this.product);
-      this.productService.addProduct(this.product).subscribe({
-        next: (products) => {
-          console.log('Product added successfully:', products);
-        },
-        error: (err) => {
-          console.error('Error adding product:', err);
-        },
-      });
+  onSubmit() {
+    if (this.productForm.valid) {
+      console.log('Form Submitted:', this.productForm.value);
+      // Xử lý thêm sản phẩm tại đây
     }
   }
 }
